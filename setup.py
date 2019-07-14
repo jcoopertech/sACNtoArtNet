a1c26e3fa3938cea6ba8182553a4084221773018
niggiover9000 <nicolasbartels@web.de>
2019-07-14 20:35:56 +0200

    Created setup file for easier global variable access

---
+++ setup.py
@@ -0,0 +1,21 @@
+import socket_settings
+
+
+def calculate_hibit(byte: int):
+    hibyte = (byte >> 8)
+    lobyte = (byte & 0xFF)
+    return hibyte, lobyte
+
+
+"""SETUP"""
+# Set up Socket for ArtNet and sACN
+set_acn_sock = socket_settings.sacn_socket_setup(socket_settings.ip)
+set_artnet_sock = socket_settings.artnet_socket_setup(socket_settings.ip)
+
+universe_shift = {}  # Create an empty dict for the shift function
+merge_dict = {}  # Create an empty dict for the merge function
+for universes in range(socket_settings.universe_max + 1):  # It is as long as the highest possible universe.
+    universe = calculate_hibit(universes)
+    merge_dict[universe[0], universe[1]] = {}
+for elements in merge_dict:
+    universe_shift = {elements: elements}
